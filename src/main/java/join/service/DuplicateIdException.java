ddf0919b195afc4fb1460e261d813c71b8525ef5
pendrakez <kamelot225@gmail.com>
2024-05-17 21:31:44 +0900

    Resolve merge conflicts

--- src/main/java/join/service/DuplicateIdException.java
+++ src/main/java/join/service/DuplicateIdException.java
@@ -2,10 +2,10 @@ package join.service;
 
 public class DuplicateIdException extends RuntimeException {
     public DuplicateIdException() {
-        super();
+        super("아이디가 중복되었습니다.");
     }
 
     public DuplicateIdException(String message) {
-        super(message); // 추가 필요
+        super(message);
     }
 }
